d4b3e7cbee972315d55755489bfde6923741ea04
GODZILLA <85907312+GODZILLA66640@users.noreply.github.com>
2022-04-09 00:40:26 +0100

    Update my dad does herobrine.bat

--- my dad does herobrine.bat
+++ my dad does herobrine.bat
@@ -17,17 +17,11 @@ call :colorEcho 0f "1."
 call :colorEcho 0f " Porn"
 call :colorEcho c0 "hub"
 echo.
-call :colorEcho 0f "2."
-call :colorEcho 0f " Scro"
-call :colorEcho 04 "lll"
-call :colorEcho 0f "er"
-echo.
 
 set choice=
 set /p choice=
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='1' goto 1
-if '%choice%'=='2' goto 2
 goto start
 
 :1
@@ -38,28 +32,6 @@ timeout 10 >nul
 %SendKeys% "YOUR USERNAMEEEEEEEEEEeee"
 %SendKeys% "{TAB}"
 %SendKeys% "YOUR PASSWORDddddddddddddddddddddddddddddddddddddddddddddddddddd"
-%SendKeys% "{ENTER}"
-del SendKeys.js
-exit
-
-:2
-cls
-START chrome -guest https://scrolller.com/discover
-echo new ActiveXObject("WScript.Shell").AppActivate("chrome") >nul
-timeout 10 >nul
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{TAB}"
-%SendKeys% "{ENTER}"
-%SendKeys% "{TAB}"
 %SendKeys% "{ENTER}"
 del SendKeys.js
 exit
